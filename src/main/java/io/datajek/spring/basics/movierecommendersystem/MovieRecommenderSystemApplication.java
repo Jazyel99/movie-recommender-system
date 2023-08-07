027c199f0aa83c2a9820a88024e616a83ad1ca61
Jazyel99 <jaziel@outlook.com>
2023-08-07 13:46:07 -0300

    feat: created instance of RecommenderImplementetation and print recommender movies

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -1,13 +1,20 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Arrays;
+
+// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lesson1.RecommenderImplementation;
 
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+		// SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+		RecommenderImplementation recommender = new RecommenderImplementation();
+		String[] result = recommender.recommendMovies("Finding Dory");
+		System.out.println(Arrays.toString(result));
 	}
 
 }
